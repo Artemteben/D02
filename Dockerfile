0e91a88735ad974c589985d01421feabe606281a
Artemteben <artyom.teben@yandex.ru>
2025-03-30 18:01:28 +0300

    app

--- Dockerfile
+++ Dockerfile
@@ -17,3 +17,5 @@ RUN pip install -r /requirements.txt --no-cache-dir
 COPY . .
 
 EXPOSE 8000
+
+CMD ("sh", "-c","python manage.py collectstatic --noinput && gunicorn config.wsgi:application --bind 0.0.0.0:8000")
--- config/settings.py
+++ config/settings.py
@@ -61,6 +61,16 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# if 'test' in sys.argv:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'test_db.sqlite3',
+#         }
+#     }
+# else:
+
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
